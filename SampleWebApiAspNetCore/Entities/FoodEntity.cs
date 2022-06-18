45df2ab541cca2685486b13b53d0ab69ee466708
VOLODYMUR-PC\Volodymur <volodymur.semenets@gmail.com>
2022-06-18 23:29:37 +0300

    New entities

--- SampleWebApiAspNetCore/Entities/FoodEntity.cs
+++ SampleWebApiAspNetCore/Entities/FoodEntity.cs
@@ -10,4 +10,19 @@ namespace SampleWebApiAspNetCore.Entities
         public int Calories { get; set; }
         public DateTime Created { get; set; }
     }
+
+    public class Root
+    {
+        public string item_id { get; set; }
+        public string city { get; set; }
+        public int quality { get; set; }
+        public int sell_price_min { get; set; }
+        public DateTime sell_price_min_date { get; set; }
+        public int sell_price_max { get; set; }
+        public DateTime sell_price_max_date { get; set; }
+        public int buy_price_min { get; set; }
+        public DateTime buy_price_min_date { get; set; }
+        public int buy_price_max { get; set; }
+        public DateTime buy_price_max_date { get; set; }
+    }
 }
